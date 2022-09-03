fd0132eda5b642aaca4de8eb16a3c970ab6cf9d2
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-09-03 13:09:53 +0100

    Made it good

---
+++ pages/api/wizard.ts
@@ -0,0 +1,60 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Message } from '../../utils/types'
+const { Configuration, OpenAIApi } = require('openai')
+
+const Prompt = `How likely is person 2 romantically interested in person 1? (Definitely, Most likely, Can't Tell, Probably don't, or Definitely don't)
+
+person 1: hey
+person 2: hey
+person 1: how was your day
+person 2: fine
+answer: Probably don't
+
+person 1: hey
+person 2: I love you
+answer: Definitely
+
+person 1: How was your day?
+person 2: ok
+answer: Can't Tell
+
+person 2: Hey
+person 1: Hi, how are you?
+person 2: fine
+person 1: wyd?
+person 2: nothing
+person 1: How was your day?
+person 2: fine 
+answer: Probably don't
+
+`
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const messages: Message[] = JSON.parse(req.body).messages
+  const formattedMessages = messages
+    .map(
+      (message) =>
+        (message.from == 'you' ? 'person 1: ' : 'person 2: ') + message.message
+    )
+    .join('\n')
+
+  const wholePrompt = Prompt + formattedMessages + '\n' + 'answer:'
+  console.log(wholePrompt)
+
+  const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+  })
+  const openai = new OpenAIApi(configuration)
+  const response: any = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: wholePrompt,
+    max_tokens: 5,
+    temperature: 0.5,
+  })
+  const answer = response.data.choices[0].text.trim()
+  res.status(200).json({ answer: answer })
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,127 @@
-import Response from './Response'
+import { useState } from 'react'
+import { Message } from '../utils/types'
 
-const Question = () => {
+const MessageView = ({
+  message,
+  onChange,
+  remove,
+}: {
+  message: Message
+  onChange: any
+  remove: any
+}) => {
   return (
-    <div className="flex flex-row py-1 px-3 items-center">
-      <p className="flex-1 text-white mr-2">You:</p>
+    <div className="flex flex-row py-1 px-3 items-center w-full">
+      <p className="flex-1 text-white">{message.from}:</p>
       <input
         type="text"
-        placeholder="Ask her anything..."
-        className="flex-3 bg-black border-b border-[#32373e]"
-      ></input>
+        placeholder="Message..."
+        className="flex-[4] bg-black border-b border-[#32373e] focus:outline-none"
+        value={message.message}
+        onChange={onChange}
+      />
+      <p onClick={remove} className="ml-1 hover:cursor-pointer font-bold">
+        ｘ
+      </p>
     </div>
   )
 }
 
 export default function Home() {
+  const [messages, setMessages]: [Message[], any] = useState([])
+  const [answer, setAnswer]: [string, any] = useState('')
+  const [ratingSubmitted, setRatingSubmitted]: [boolean, any] = useState(false)
+  const ratings = ['Great', 'Good', 'Ok', 'Bad', 'Wrong']
+
+  const fetchAnswer = async () => {
+    setRatingSubmitted(false)
+    setAnswer('Loading...')
+    const res = await fetch('/api/wizard', {
+      method: 'POST',
+      body: JSON.stringify({ messages: messages }),
+    })
+    const data = await res.json()
+    setAnswer(data.answer)
+  }
+  const submitRating = (rating: string) => {
+    setRatingSubmitted(true)
+  }
   return (
     <div className="flex flex-col w-full h-full my-8 items-center">
-      <h1 className="text-center font-bold text-6xl mb-5">Is She Into You?</h1>
+      <h1 className="text-center font-bold text-6xl mb-5">Do They Like You?</h1>
       <h2 className="text-center text-2xl mb-5">
-        GPT-3 can decide if she is into you, based on a text conversation you
-        have had!
+        Decipher their mixed signals and cryptic messages
       </h2>
       {/* <div className="max-w-[82] bg-[#2b2031] h-24 rounded-lg"></div> */}
-      <div className="border-[#32373e] border rounded-xl py-1">
-        <Question />
-        <Question />
-        <div className="flex flex-row mx-2">
-          {/* MAKE BG SWITCH FROM ACTIVE (PURPLE) to not active (grey) based on last added */}
-          <p className="flex-1 text-center rounded-lg py-1 mr-1 bg-[#32243d]">
-            + You
-          </p>
-          <p className="flex-1 text-center rounded-lg py-1 ml-1 bg-[#2b2031]">
-            + Them
-          </p>
+      <div className="flex flex-col items-center w-96 px-4">
+        <div className="border-[#32373e] border rounded-xl py-1 w-full">
+          {messages.map((message, idx) => (
+            <MessageView
+              message={message}
+              key={idx}
+              remove={() =>
+                setMessages([
+                  ...messages.slice(0, idx),
+                  ...messages.slice(idx + 1, messages.length),
+                ])
+              }
+              onChange={(e: any) =>
+                setMessages(
+                  Object.assign([], {
+                    ...messages,
+                    [idx]: { ...message, message: e.target.value },
+                  })
+                )
+              }
+            />
+          ))}
+          <div className="flex flex-row mx-2 mt-2 mb-1">
+            {['you', 'them'].map((x) => (
+              <p
+                className={`flex-1 text-center rounded-lg py-1 mx-1 bg-[#32243d] hover:cursor-pointer`}
+                key={x}
+                onClick={() =>
+                  setMessages([...messages, { from: x, message: '' }])
+                }
+              >
+                + {x}
+              </p>
+            ))}
+          </div>
         </div>
+
+        <button
+          className="flex-1 text-center rounded-lg p-3 my-5 ml-1 bg-[#32243d] w-11/12"
+          onClick={fetchAnswer}
+        >
+          Find Out Their True Feelings...
+        </button>
+        {answer == 'Loading...' || answer == "Can't Tell" ? (
+          <p className="font-bold text-4xl text-center">{answer}</p>
+        ) : answer ? (
+          <>
+            <p className="mb-5 text-4xl text-center">
+              They <span className="font-bold">{answer}</span> like you
+            </p>
+            {!ratingSubmitted && (
+              <>
+                <h3 className="text-2xl text-center mb-2">Rate this answer</h3>
+                <div className="flex flex-row">
+                  {ratings.map((x) => (
+                    <p
+                      key={x}
+                      className="flex-1 text-center mx-1 py-1 px-2 border border-[#32373e] rounded-xl hover:cursor-pointer"
+                      onClick={() => submitRating(x)}
+                    >
+                      {x}
+                    </p>
+                  ))}
+                </div>
+              </>
+            )}
+          </>
+        ) : null}
       </div>
-      <Response />
     </div>
   )
 }
