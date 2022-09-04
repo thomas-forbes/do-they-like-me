2962c2a844b12b7d991659c3a1329d60a8762af9
ChickenTaxi <willcarkner2006@gmail.com>
2022-09-04 12:44:11 +0100

    fixed thomas' dumbass

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,16 +4,17 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        <title>Do They Like You?</title>
-        <Script
+      <Head>
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9711051658997865"
           crossOrigin="anonymous"
         />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <title>Do They Like You?</title>
       </body>
     </Html>
   )
